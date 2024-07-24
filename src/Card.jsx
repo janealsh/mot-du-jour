970dba4d62419d699fa9d9441614c7ed534f3d7a
Jane Al-Shihabi <janealshihabi@gmail.com>
2024-07-23 21:06:39 -0400

    large commit for the project setup and styling

---
+++ src/Card.jsx
@@ -0,0 +1,17 @@
+
+
+const Card = () => {
+  return <div className="card">
+        <h2 className="card-title">Conspicuous</h2>
+        <p className="card-body">/kənˈspɪk.ju.əs/</p>
+        <p className="card-body">Noun</p>
+        <p className="card-body">1. Obvious or easy to notice.</p>
+        <p className="example-sentence">She was <strong>conspicuous</strong> by their absence.</p>
+        <p className="card-body">2. Noticeable or attracting attention, especially if unattractive.</p>
+        <p className="etymology"><strong>Etymology:</strong> From Latin conspicuus (“visible, striking”), from cōnspicere (“to notice”), from con- (“with, together”) + specere (“to look at”).</p>
+
+
+  </div>;
+};
+
+export default Card;
---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return <div>
+    <header className="custom-header">
+      <h1 className="header-title">Mot du Jour</h1>
+    </header>
+  </div>;
+};
+
+export default Header;
